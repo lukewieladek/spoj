50ab762e88586948db11e576a4841ce9c897b3d2
Łukasz Wielądek <wieladek.lukasz@gmail.com>
2015-10-28 14:45:24 +0100

    java io

---
+++ src/s_0000_java_io/Nieokreslona_Liczba_Testow.java
@@ -0,0 +1,19 @@
+package s_0000_java_io;
+
+import java.util.Scanner;
+
+/**
+ * Created by £ukasz on 2015-09-17.
+ */
+public class Nieokreslona_Liczba_Testow
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext())
+        {
+            int x = scanner.nextInt();
+            System.out.println(x);
+        }
+    }
+}
---
+++ src/s_0000_java_io/Wczytywanie_i_Wypisywanie_Dwoch_Liczb.java
@@ -0,0 +1,9 @@
+package s_0000_java_io;
+
+/**
+ * Created by £ukasz on 2015-09-17.
+ */
+public class Wczytywanie_i_Wypisywanie_Dwoch_Liczb
+{
+
+}
