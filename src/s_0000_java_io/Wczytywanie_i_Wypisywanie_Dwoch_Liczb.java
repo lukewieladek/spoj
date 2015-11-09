476d49235239e58d591c98f3649a2881bd9875d5
Łukasz Wielądek <wieladek.lukasz@gmail.com>
2015-11-09 20:13:05 +0100

    update

--- src/s_0000_java_io/Wczytywanie_i_Wypisywanie_Dwoch_Liczb.java
+++ src/s_0000_java_io/Wczytywanie_i_Wypisywanie_Dwoch_Liczb.java
@@ -1,9 +1,20 @@
 package s_0000_java_io;
 
+import java.util.Scanner;
+
 /**
  * Created by £ukasz on 2015-09-17.
  */
 public class Wczytywanie_i_Wypisywanie_Dwoch_Liczb
 {
-
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext())
+        {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            System.out.printf("%d %d\n", x, y);
+        }
+    }
 }
