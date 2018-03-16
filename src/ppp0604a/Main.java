ce27bf20b260991c818ee0510301312e7ebfb784
Łukasz Wielądek <wieladek.lukasz@gmail.com>
2018-03-17 00:20:16 +0100

    update

--- src/ppp0604a/Main.java
+++ src/ppp0604a/Main.java
@@ -4,43 +4,40 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
-		
+
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		short iloscZadan = Short.parseShort(in.readLine());
-		
-		
-		while (iloscZadan > 0) {
-			String[] strTab = in.readLine().split("\\s+");
-			
-			short n = (short)strTab.length;
-			
-			short[] tab = new short[n];
-			
-			int suma = 0;
-			
-			for (short i = 0; i < n; ++i) {
-				tab[i] = Short.parseShort(strTab[i]);
-				suma += tab[i];
-			}
-			
-			float srednia = (float)suma / n;
-			
-			short szukana = tab[0];
 
-			for (short i = 1; i < n; ++i) {
-				if (szukana == srednia) break;
-				if (Math.abs(srednia - tab[i]) < Math.abs(srednia - szukana))
-					if (Math.abs(szukana - srednia) != Math.abs(tab[i] - srednia))
-						if (Math.abs(szukana - srednia) > 1 && Math.abs(szukana - srednia) > 1)
-							szukana = tab[i];
-			}
-			
+		while (iloscZadan > 0) {
+			String[] strTab = in.readLine().split("\\s+"); // read all line with number and digits
+
+            short n = Short.parseShort(strTab[0]); // number of digits
+
+            short[] tab = new short[n];
+
+            for (int i = 0; i < n; i++) {
+                tab[i] = Short.parseShort(strTab[i + 1]);
+            }
+
+            int suma = 0;
+
+            for (int i = 0; i < n; i++)
+                suma += tab[i];
+
+            float srednia = (float) suma / n;
+
+            short szukana = tab[0];
+
+            for (int i = 1; i < n; i++) {
+                if (Math.abs(srednia - tab[i]) < Math.abs(srednia - szukana))
+                {
+                    szukana = tab[i];
+                }
+            }
+
 			System.out.println(szukana);
 			
 			--iloscZadan;
 		}
-		
-		System.exit(0);
 	}
 }
