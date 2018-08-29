6eea8f0ab2686ea4386891931b558f3841837f31
Łukasz Wielądek <wieladek.lukasz@gmail.com>
2018-08-29 16:07:36 +0200

    update

--- src/pp0602d/Main.java
+++ src/pp0602d/Main.java
@@ -3,6 +3,9 @@ package pp0602d;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
@@ -16,8 +19,16 @@ public class Main {
 
         String[] numbers = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
+        List<String> numberList = Arrays.asList(numbers);
+
         int przesuniecie = k % n;
 
-        System.out.println("Przesuniecie " + przesuniecie);
+        Collections.rotate(numberList, -k);
+
+        for (String number : numberList) {
+            System.out.print(number + " ");
+        }
+
+        System.out.println();
     }
 }
