2c02c72a9fbcd87ca0c95bde894ac2d36a9a22b8
Łukasz Wielądek <wieladek.lukasz@gmail.com>
2018-08-31 12:46:20 +0200

    update

--- src/pp0602b/Main.java
+++ src/pp0602b/Main.java
@@ -12,8 +12,39 @@ public class Main {
 
         int t = Integer.parseInt(line[0]);
 
-        for (int i = 0; i < t; i++) {
+        for (int n = 0; n < t; n++) {
 
+            line = bufferedReader.readLine().replaceAll("\\s+$", " ").split(" ");
+
+            int l = Integer.parseInt(line[0]);
+            int k = Integer.parseInt(line[1]);
+
+            String[][] number = new String[l][k];
+
+            for (int i = 0; i < l; i++) {
+                line = bufferedReader.readLine().replaceAll("\\s+$", " ").split(" ");
+
+                for (int j = 0; j < k; j++) {
+
+                    // pierwsza kolumna, dowolny wierz - ale nie ostatni
+
+                    // ostatni wiersz
+
+                    //
+
+
+                    if (j == 0 && i != l - 1)
+                        number[i+1][j] = line[j];
+                    else if (i == l - 1)
+                        number[i][j + 1] = line[j];
+                    else
+                        number[i][j] = line[j];
+
+
+                    System.out.print(number[i][j] + " ");
+                }
+                System.out.println();
+            }
         }
     }
 }
